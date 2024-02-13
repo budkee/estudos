4cdeee18605ae270828d64e934e4aa90ac50e065
budkee <camila.budke@gmail.com>
2024-02-13 03:11:55 -0400

    Organização do repositório #01

--- comp_distribuida/Python/TCP/cliente.py
+++ comp_distribuida/Python/TCP/cliente.py
@@ -1,29 +1,30 @@
+# 0. Imports
 import socket
 
-# Configurar o host e a porta
+# 1. Configurar o host e a porta
 host = '192.168.100.113' ## IP da máquina que deseja conectar
 port = 7777  ## Acima de 5000
 
-# Criar um objeto socket
+# 2. Criar um objeto socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Conectar ao servidor
+# 3. Conectar ao servidor
 client_socket.connect((host, port))
 print('Conectado ao servidor')
 
-# Realizar a comunicação com o servidor
+# 4. Realizar a comunicação com o servidor
 while True:
-    # Enviar mensagem ao servidor
+    # 4.1. Enviar mensagem ao servidor
     message = input('Digite uma mensagem (Ou "sair" para sair): ')
     client_socket.send(message.encode())
     
-    # Receber resposta do servidor
+    # 4.2 Receber resposta do servidor
     response = client_socket.recv(1024).decode()
     print('Servidor:', response)
 
-    # Verifica se o usuário deseja sair
-    if message.lower() == 'sair':
+    # 4.3. Verifica se o usuário deseja sair
+    if message.lower() == 'q':
         break
 
-# Encerrar a conexão
+# 5. Encerrar a conexão
 client_socket.close()
