079a13ec7584b76c67877a462783bd6062de700f
budkee <camila.budke@gmail.com>
2024-02-21 17:38:52 -0400

    Computação Distribuída | Multithreading #01

---
+++ comp_distribuida/Python/multithreading_cliente.py
@@ -0,0 +1,9 @@
+""""
+    Código desenvolvido a partir do site: https://awari.com.br/thread-em-python-aprenda-a-criar-programas-multi-threading-com-python/?utm_source=blog&utm_campaign=projeto+blog&utm_medium=Thread%20em%20Python:%20Aprenda%20a%20criar%20programas%20multi-threading%20com%20Python
+"""
+# 0. Imports
+import threading
+# 1. Entradas
+
+# 2. Processamento
+# 3. Saídas
