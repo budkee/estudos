a431183ec1f57328858580fa38f3cad8edd39267
budkee <kae.budke@gmail.com>
2024-09-20 15:09:21 -0400

    Ajustes Repo

--- fim_de_expediente.sh
+++ fim_de_expediente.sh
@@ -5,7 +5,7 @@
 # Alterar a mensagem conforme o que foi feito no dia 
 
 git add .
-git commit -m "Estrutura de dados - Árvores Binárias e Percursos | #01"
+git commit -m "Ajustes Repo"
 echo "\n"
 git push
 echo "\n"
