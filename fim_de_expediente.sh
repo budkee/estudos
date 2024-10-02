ae0d564981b13b57579ea9c9fd5836c6f7e3c416
budkee <kae.budke@gmail.com>
2024-10-02 11:33:52 -0400

    Organização Repo | 02/10/24

--- fim_de_expediente.sh
+++ fim_de_expediente.sh
@@ -5,7 +5,7 @@
 # Alterar a mensagem conforme o que foi feito no dia 
 
 git add .
-git commit -m "Contagem de objetos para sistema de segurança | 27/09/24"
+git commit -m "Organização Repo | 02/10/24"
 echo "\n"
 git push
 echo "\n"
