5b608d6ffe36231b07b7628f909192ba53fd8b7e
Kaê <kae.budke@gmail.com>
2024-12-31 19:37:07 -0300

    Add fim_de_expediente.sh

---
+++ devOps/scripts/fim_de_expediente.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# 
+# Repositório no GitHub: https://github.com/budkee/<repo>
+#
+# Alterar a mensagem conforme o que foi feito no dia 
+
+git add .
+git commit -m "Aula de LBD - Segurança | 11/11/24"
+echo "\n"
+git push
+echo "\n"
+git status
+
