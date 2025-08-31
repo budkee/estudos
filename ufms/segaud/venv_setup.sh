4a0d9ba4f4ac2b96feb9a16b31bba8ee80f98108
Kaê <kae.budke@gmail.com>
2025-08-31 00:38:37 -0400

    Guia Prático | vi

--- ufms/segaud/venv_setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-# Execute o script com o comando: source venv_setup.sh
-# Para sair do ambiente virtual, execute o comando: deactivate
-python3 -m venv .venv
-source .venv/bin/activate   
-python3 -m ensurepip --upgrade
-pip install --upgrade pip
-pip list
